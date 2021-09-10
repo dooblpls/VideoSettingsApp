6953010abb81c2e8d45632778d2aa93cac7ee99d
dooblpls <dooblpls@users.noreply.github.com>
2021-09-10 11:44:32 +0200

    Projektdateien hinzufügen.

---
+++ VideoSettingsApp/Form1.cs
@@ -0,0 +1,124 @@
+﻿using DirectShowLib;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace VideoSettingsApp
+{
+    public partial class Form1 : Form
+    {
+        [DllImport("oleaut32.dll", CharSet = CharSet.Unicode, ExactSpelling = true)]
+        public static extern int OleCreatePropertyFrame(
+            IntPtr hwndOwner,
+            int x,
+            int y,
+            [MarshalAs(UnmanagedType.LPWStr)] string lpszCaption,
+            int cObjects,
+            [MarshalAs(UnmanagedType.Interface, ArraySubType=UnmanagedType.IUnknown)]
+            ref object ppUnk,
+            int cPages,
+            IntPtr lpPageClsID,
+            int lcid,
+            int dwReserved,
+            IntPtr lpvReserved);
+        IBaseFilter theDevice = null;
+        private void DisplayPropertyPage(IBaseFilter dev)
+        {
+            //Get the ISpecifyPropertyPages for the filter
+            ISpecifyPropertyPages pProp = dev as ISpecifyPropertyPages;
+            int hr = 0;
+
+            if (pProp == null)
+            {
+                //If the filter doesn't implement ISpecifyPropertyPages, try displaying IAMVfwCompressDialogs instead!
+                IAMVfwCompressDialogs compressDialog = dev as IAMVfwCompressDialogs;
+                if (compressDialog != null)
+                {
+
+                    hr = compressDialog.ShowDialog(VfwCompressDialogs.Config, IntPtr.Zero);
+                    DsError.ThrowExceptionForHR(hr);
+                }
+                return;
+            }
+
+            //Get the name of the filter from the FilterInfo struct
+            FilterInfo filterInfo;
+            hr = dev.QueryFilterInfo(out filterInfo);
+            DsError.ThrowExceptionForHR(hr);
+
+            // Get the propertypages from the property bag
+            DsCAUUID caGUID;
+            hr = pProp.GetPages(out caGUID);
+            DsError.ThrowExceptionForHR(hr);
+
+            // Create and display the OlePropertyFrame
+            object oDevice = (object)dev;
+            hr = OleCreatePropertyFrame(this.Handle, 0, 0, filterInfo.achName, 1, ref oDevice, caGUID.cElems, caGUID.pElems, 0, 0, IntPtr.Zero);
+            DsError.ThrowExceptionForHR(hr);
+
+            // Release COM objects
+            Marshal.FreeCoTaskMem(caGUID.pElems);
+            Marshal.ReleaseComObject(pProp);
+            if (filterInfo.pGraph != null)
+            {
+                Marshal.ReleaseComObject(filterInfo.pGraph);
+            }
+        }
+        private IBaseFilter CreateFilter(Guid category, string friendlyname)
+        {
+            object source = null;
+            Guid iid = typeof(IBaseFilter).GUID;
+            foreach (DsDevice device in DsDevice.GetDevicesOfCat(category))
+            {
+                if (device.Name.CompareTo(friendlyname) == 0)
+                {
+                    device.Mon.BindToObject(null, null, ref iid, out source);
+                    break;
+                }
+            }
+
+            return (IBaseFilter)source;
+        }
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            foreach (DsDevice ds in DsDevice.GetDevicesOfCat(FilterCategory.VideoInputDevice))
+            {
+                comboBox1.Items.Add(ds.Name);
+            }
+            if (comboBox1.Items.Count > 0)
+            {
+                comboBox1.SelectedIndex = 0;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DisplayPropertyPage(theDevice);
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            //Release COM objects
+            if (theDevice != null)
+            {
+                Marshal.ReleaseComObject(theDevice);
+                theDevice = null;
+            }
+            //Create the filter for the selected video input device
+            string devicepath = comboBox1.SelectedItem.ToString();
+            theDevice = CreateFilter(FilterCategory.VideoInputDevice, devicepath);
+        }
+    }
+}
